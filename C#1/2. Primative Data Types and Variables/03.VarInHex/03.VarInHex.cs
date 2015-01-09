01f6c86e6d6758ce6188408ccf289cba2b666061
jumperabg <krasimirvelichkov@gmail.com>
2015-01-09 18:27:55 +0200

    QUick update

---
+++ C#1/2. Primative Data Types and Variables/03.VarInHex/03.VarInHex.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class varInHex
+{
+    static void Main(string[] args)
+    {
+        int var = 0xFE;
+        Console.WriteLine(var);
+    }
+}
+
---
+++ C#1/2. Primative Data Types and Variables/04.UnicodeChar/04.UnicodeChar.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char star = '\u002A';
+        Console.WriteLine(star);
+    }
+}
