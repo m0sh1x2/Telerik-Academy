7b618e394927958a18e45c9afc44a68844f25f6d
Krasimir Iliev Velichkov <krasimirvelichkov@gmail.com>
2015-01-13 15:19:51 +0200

    Updating loops

---
+++ C#1/6.Loops/14.DecimalToBinaryFixed/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.DecimalToBinaryFixed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(n.ToString("X"));
+        }
+
+        public static string convert(string n)
+        {
+            string result = string.Empty;
+            switch (n)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    result = n;
+                    break;
+                case "10":
+                    result = "A";
+                    break;
+                case "11":
+                    result = "B";
+                    break;
+                case "12":
+                    result = "C";
+                    break;
+                case "13":
+                    result = "D";
+                    break;
+                case "14":
+                    result = "E";
+                    break;
+                case "15":
+                    result = "F";
+                    break;
+                default:
+                    result = "0";
+                    break;
+            }
+            return result;
+
+        }
+    }
+
+}
---
+++ C#1/6.Loops/15.HexToDec/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.HexToDec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+
+            Console.WriteLine(Convert.ToInt32(n,16));
+        }
+    }
+}
