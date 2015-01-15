3b9d14077e4acf561f1d558908974f6bfad538db
Krasimir Iliev Velichkov <krasimirvelichkov@gmail.com>
2015-01-15 15:07:39 +0200

    one more task

---
+++ C#1/6.Loops/18.TrailingZeroesOfFactoriel/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace _18.TrailingZeroesOfFactoriel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            int power = 5;
+            do
+            {
+                sum += (input / 5);
+            } while (power < 5);
+            Console.WriteLine(sum);
+
+        }
+
+    }
+}
---
+++ C#1/6.Loops/19.SpiralMatrix/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.SpiralMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n,n];
+            int counter = 1;
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+
+                    matrix[col, row] = counter;
+                    counter++;
+                }
+            }
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    Console.Write("{0,-4}", matrix[col, row]);
+
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
