c92b315eecdd4030965f0fa11207ead442ac9251
jumperabg <krasimirvelichkov@gmail.com>
2015-01-21 21:50:37 +0200

    edited folders

---
+++ C#1/Exam Tasks Preperation/Exam 2013-12-05/02.AngryFemaleGPS/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        long inputNumber = long.Parse(Console.ReadLine());
+        if (inputNumber < 0)
+        {
+            inputNumber = -inputNumber;
+        }
+        int oddDigits = 0;
+        int evenDigits = 0;
+
+        while (inputNumber > 0)
+        {
+            byte digit = (byte)(inputNumber % 10);
+            if (digit % 2 == 0)
+            {
+                evenDigits += digit;
+            }
+            else
+            {
+                oddDigits += digit;
+            }
+            inputNumber /= 10;
+        }
+
+        if (oddDigits > evenDigits)
+        {
+            Console.WriteLine("left {0}", oddDigits);
+        }
+        else if (evenDigits > oddDigits)
+        {
+            Console.WriteLine("right {0}", evenDigits);
+        }
+        else if (evenDigits == oddDigits)
+        {
+            Console.WriteLine("straight {0}", oddDigits);
+        }
+
+    }
+}
