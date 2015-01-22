9fc8f5638931b6a9592bc6832255b4c675db0d0f
jumperabg <krasimirvelichkov@gmail.com>
2015-01-23 01:31:30 +0200

    Added math Expression

---
+++ C#1/Exam Tasks Preperation/Exam 1 27 dec 2012/01.MathExpression/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.MathExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Threading.Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+
+            double N = double.Parse(Console.ReadLine());
+            double M = double.Parse(Console.ReadLine());
+            double P = double.Parse(Console.ReadLine());
+
+
+            double result, sin;
+            
+            result = (N*N + (1.0/(M*P)) + 1337.0);
+            result /= N - 128.523123123*P;
+            result += Math.Sin((int)M%180);
+
+            Console.WriteLine("{0:F6}",result);
+
+        }
+    }
+}
