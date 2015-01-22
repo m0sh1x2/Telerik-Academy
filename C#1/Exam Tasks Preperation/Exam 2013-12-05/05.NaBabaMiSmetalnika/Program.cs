080dcf3664f7f02baca04475de5c15be49e5c73e
jumperabg <krasimirvelichkov@gmail.com>
2015-01-22 18:04:22 +0200

    Added more tasks

--- C#1/Exam Tasks Preperation/Exam 2013-12-05/05.NaBabaMiSmetalnika/Program.cs
+++ C#1/Exam Tasks Preperation/Exam 2013-12-05/05.NaBabaMiSmetalnika/Program.cs
@@ -10,7 +10,41 @@ namespace _05.NaBabaMiSmetalnika
     {
         static void Main(string[] args)
         {
-            int wid
+            int[] matrix = new int[8];
+
+
+            // input
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                matrix[i] = int.Parse(Console.ReadLine());
+            }
+
+            // solution
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int line = 7; line > 0; line--)
+                {
+                    for (int bitPosition = 0; bitPosition < matrix.Length; bitPosition++)
+                    {
+                        if ((matrix[line] >> bitPosition & 1) == 0 && (matrix[line - 1] >> bitPosition & 1) == 1)
+                        {
+                            matrix[line] |= (1 << bitPosition);
+                            matrix[line - 1] ^= (1 << bitPosition);
+                        }
+                    }
+                }
+            }
+
+
+     
+
+            // output
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                Console.WriteLine(matrix[i]);
+            }
+
         }
     }
 }
