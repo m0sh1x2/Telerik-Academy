c88e342918fdd537809b1691fc24d4ec0f2677b6
jumperabg <krasimirvelichkov@gmail.com>
2015-02-22 14:16:36 +0200

    Adding more tasks from old exams !

--- C#2/CSharpTwoExamPreparation/00.Testing/Program.cs
+++ C#2/CSharpTwoExamPreparation/00.Testing/Program.cs
@@ -1,214 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Globalization;
+using System.Threading.Tasks;
 
-struct Element
+namespace Example
 {
-    // define new object type with structure
-    // screen position x,y
-    public int x;
-    public int y;
-    // type of game object is defined by ASCII char
-    public char skin;
-    public ConsoleColor colour;
-}
-
-class PacMan3D
-{
-    static void Main()
+    class Program
     {
-        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
-
-        #region Memory Initialization
-
-        // set console size (screen resolution)
-        Console.BufferHeight = Console.WindowHeight = 21;
-        Console.BufferWidth = Console.WindowWidth = 40;
-
-        // set playfield size
-        int playfieldHeight = Console.WindowHeight - 1;
-        int playfieldWidth = Console.WindowWidth - 20;
-
-        // define hero pacMan as a variable of type element
-        Element pacMan = new Element();
-        // initial pacman position in center of playfield
-        pacMan.x = playfieldWidth / 2;
-        pacMan.y = playfieldHeight / 2;
-        pacMan.skin = (char)9787; // utf8 decimal code 9787 (smile face) is our hero character
-        pacMan.colour = ConsoleColor.Yellow;
-
-
-
-
-        //making list for multiple enemies
-        List<Element> BadGuys = new List<Element>();
-        // define enemies as a variable of type element
-        Element newBadGuy = new Element();
-        for (int i = 0; i < 7; i++)
+        static void Main(string[] args)
         {
-            // initial badguy position on the playfield playfield  
-            Random randomizerBadGuy = new Random();
-            int positionBadGuyX = randomizerBadGuy.Next(1, 6);
-            int positionBadGuyY = randomizerBadGuy.Next(1, 6);
-            newBadGuy.x = playfieldHeight / positionBadGuyX;
-            newBadGuy.y = playfieldWidth / positionBadGuyY;
-            newBadGuy.skin = '\u2666'; // utf8 decimal code 2736 (black star) is our enemy character
-            newBadGuy.colour = ConsoleColor.Red;
-            BadGuys.Add(newBadGuy);
-        }
-        // define labyrinth variable and build example
-        string[] labyrinth = new string[playfieldHeight];
+            Grasshoper();
 
-        Random rand = new Random();
-
-
-        for (int row = 0; row < playfieldHeight; row++)
-        {
-            for (int col = 0; col < playfieldWidth; col++)
-            {
-
-                if (row == playfieldHeight / 2 && col == playfieldWidth / 2)
-                {
-                    labyrinth[row] += " ";
-                    continue;
-                }
-                int chance = rand.Next(1, 101);
-                if (chance < 20)
-                {
-                    labyrinth[row] += "\u2588";
-                }
-                else
-                {
-                    labyrinth[row] += " ";
-                }
-
-            }
         }
 
-        #endregion
-
-        while (true)
+        private static void Grasshoper()
         {
-
-
-            #region Build Frame
-
-            //badguy move pathern
-            Random switchBadGuy = new Random();
-            int switched = switchBadGuy.Next(4);
-            switch (switched)
-            {
-                case 0:
-                    if (newBadGuy.x > 1)
-                    {
-                        if (labyrinth[newBadGuy.y][newBadGuy.x - 1] != '\u2588')
-                        {
-                            newBadGuy.x = newBadGuy.x - 1;
-                        }
-                    }
-                    break;
-                case 1:
-                    if (newBadGuy.x < playfieldWidth - 1)
-                    {
-                        if (labyrinth[newBadGuy.y][newBadGuy.x + 1] != '\u2588')
-                        {
-
-                            newBadGuy.x = newBadGuy.x + 1;
-                        }
-                    }
-                    break;
-                case 2:
-                    if (newBadGuy.y > 1)
-                    {
-                        if (labyrinth[newBadGuy.y - 1][newBadGuy.x] != '\u2588')
-                        {
-                            newBadGuy.y = newBadGuy.y - 1;
-                        }
-                    }
-                    break;
-                case 3:
-                    if (newBadGuy.y < playfieldHeight - 1)
-                    {
-                        if (labyrinth[newBadGuy.y + 1][newBadGuy.x] != '\u2588')
-                        {
-                            newBadGuy.y = newBadGuy.y + 1;
-                        }
-                    }
-                    break;
-                default:
-                    continue;
-            }
-
-            // move PacMan
-            while (Console.KeyAvailable)
-            {
-                // we assign the pressed key value to a variable pressedKey
-                ConsoleKeyInfo pressedKey = Console.ReadKey(true);
-                // next we start checking the value of the pressed key and take action if neccessary
-                if (pressedKey.Key == ConsoleKey.LeftArrow && pacMan.x > 1) // if left arrow is pressed then
-                {
-                    if (labyrinth[pacMan.y][pacMan.x - 1] != '\u2588')
-                    {
-                        pacMan.x = pacMan.x - 1;
-                    }
-                }
-                else if (pressedKey.Key == ConsoleKey.RightArrow && pacMan.x < playfieldWidth - 1)
-                {
-                    if (labyrinth[pacMan.y][pacMan.x + 1] != '\u2588')
-                    {
-
-                        pacMan.x = pacMan.x + 1;
-                    }
-                }
-                else if (pressedKey.Key == ConsoleKey.UpArrow && pacMan.y > 1)
-                {
-                    if (labyrinth[pacMan.y - 1][pacMan.x] != '\u2588')
-                    {
-                        pacMan.y = pacMan.y - 1;
-                    }
-                }
-                else if (pressedKey.Key == ConsoleKey.DownArrow && pacMan.y < playfieldHeight - 1)
-                {
-                    if (labyrinth[pacMan.y + 1][pacMan.x] != '\u2588')
-                    {
-                        pacMan.y = pacMan.y + 1;
-                    }
-                }
-
-            }
-
-
-            #endregion
-
-            #region Print Frame
-
-            Console.Clear();    // fast screen clear
-            PrintLabyrinth(labyrinth);
-            PrintElement(pacMan);
-            PrintElement(newBadGuy);
-            #endregion
-
-            Thread.Sleep(100);  // control game speed
+            Thread.Sleep(2000);
+            Console.Beep(264, 125);
+            Thread.Sleep(250);
+            Console.Beep(264, 125);
+            Thread.Sleep(125);
+            Console.Beep(297, 500);
+            Thread.Sleep(125);
+            Console.Beep(264, 500);
+            Thread.Sleep(125);
+            Console.Beep(352, 500);
+            Thread.Sleep(125);
+            Console.Beep(330, 1000);
+            Thread.Sleep(250);
+            Console.Beep(264, 125);
+            Thread.Sleep(250);
+            Console.Beep(264, 125);
+            Thread.Sleep(125);
+            Console.Beep(297, 500);
+            Thread.Sleep(125);
+            Console.Beep(264, 500);
+            Thread.Sleep(125);
+            Console.Beep(396, 500);
+            Thread.Sleep(125);
+            Console.Beep(352, 1000);
+            Thread.Sleep(250);
+            Console.Beep(264, 125);
+            Thread.Sleep(250);
+            Console.Beep(264, 125);
+            Thread.Sleep(125);
+            Console.Beep(2642, 500);
+            Thread.Sleep(125);
+            Console.Beep(440, 500);
+            Thread.Sleep(125);
+            Console.Beep(352, 250);
+            Thread.Sleep(125);
+            Console.Beep(352, 125);
+            Thread.Sleep(125);
+            Console.Beep(330, 500);
+            Thread.Sleep(125);
+            Console.Beep(297, 1000);
+            Thread.Sleep(250);
+            Console.Beep(466, 125);
+            Thread.Sleep(250);
+            Console.Beep(466, 125);
+            Thread.Sleep(125);
+            Console.Beep(440, 500);
+            Thread.Sleep(125);
+            Console.Beep(352, 500);
+            Thread.Sleep(125);
+            Console.Beep(396, 500);
+            Thread.Sleep(125);
+            Console.Beep(352, 1000);
         }
-    }
 
-    static void PrintElement(Element thisObject)
-    {
-        // print object of type Element
-        Console.SetCursorPosition(thisObject.x, thisObject.y);
-        Console.ForegroundColor = thisObject.colour;
-        Console.Write(thisObject.skin);
-    }
 
-    static void PrintLabyrinth(string[] thisArray)
-    {
-        Console.SetCursorPosition(0, 0);
-        Console.ForegroundColor = ConsoleColor.White;
-        for (int i = 0; i < thisArray.Length; i++)
-        {
-            Console.WriteLine(thisArray[i]);
-        }
+
+
     }
 }
--- C#2/CSharpTwoExamPreparation/01.Zerg!!/Program.cs
+++ C#2/CSharpTwoExamPreparation/01.Zerg!!/Program.cs
@@ -8,63 +8,19 @@ namespace _01.Zerg__
 {
     class Program
     {
-        const int NumeralSystemBase = 15;
+        ulong numBase = 15;
+        string message = Console.ReadLine();
 
-        static int ConvertToDigit(string currentDigit)
-        {
+       
 
-            switch (currentDigit)
-            {
-                case "Rawr": return 0; break;
-                case "Rrrr": return 1; break;
-                case "Hsst": return 2; break;
-                case "Ssst": return 3; break;
-                case "Grrr": return 4; break;
-                case "Rarr": return 5; break;
-                case "Mrrr": return 6; break;
-                case "Psst": return 7; break;
-                case "Uaah": return 8; break;
-                case "Uaha": return 9; break;
-                case "Zzzz": return 10; break;
-                case "Bauu": return 11; break;
-                case "Djav": return 12; break;
-                case "Myau": return 13; break;
-                case "Gruh": return 14; break;
-                default: throw new ArgumentException();
 
-            }
 
-        }
-        static long PowerOfFifteen(int power)
-        {
-            long result = 1;
 
-            for (int i = 0; i < power; i++)
-            {
-                result *= NumeralSystemBase;
-            }
 
-            return result;
-        }
-        static void Main(string[] args)
-        {
+    }
+    public static string ConvertMessage(string message){
 
-            //Find elements in column or diagonal !
 
-            string zergMessage = Console.ReadLine();
-            int position = zergMessage.Length / 4 - 1;
-            long result = 0;
 
-            for (int i = 0; i < zergMessage.Length; i += 4)
-            {
-                string currentDigit = zergMessage.Substring(i, 4);
-
-                result += ConvertToDigit(currentDigit) * PowerOfFifteen(position);
-
-                position--;
-            }
-
-            Console.WriteLine(result);
-        }
     }
 }
